5b67099045007a861b13107b61f09199e400d5b4
Vincent Wong <vincent@vwong.dev>
2022-05-22 00:43:26 +1000

    Add ``Expression`` trait definition

---
+++ src/expression.rs
@@ -0,0 +1,11 @@
+/// Trait for expressions operating on relations in relational algebra.
+/// Implemented by operations such as Projection, Selection, Join, etc.
+/// Expressions are intended to be recursive and contain sub-expressions.
+
+pub trait Expression<S> {
+    type Output;
+
+    /// Calls evaluation on any sub-expressions before performing its 
+    /// own transformation and returning the (unwrapped) result.
+    fn eval(&self) -> Vec<Self::Output>;
+}
